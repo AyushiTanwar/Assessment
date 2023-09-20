7b4d9ba33116e6019d661f8fffe92c3170051be9
Ayushi <128356119+Poonam377@users.noreply.github.com>
2023-09-20 18:56:27 +0530

    changes

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -11,14 +11,20 @@ export default function Dashboard() {
     address: "",
   });
   const [errors, seterrors] = useState({});
-  const [list, setlist] = useState([])
+  const [list, setlist] = useState([]);
+  const deleteUser = (itm) => {
+    let l = list.filter((x) => {
+      return x.phoneNumber != itm.phoneNumber;
+    });
+    setlist(l);
+    localStorage.setItem("list", JSON.stringify(l));
+  };
 
   useEffect(() => {
-    if(localStorage.getItem('list')){
-      setlist(localStorage.getItem('list'));
+    if (localStorage.getItem("list")) {
+      setlist(JSON.parse(localStorage.getItem("list")));
     }
-  }, [])
-  
+  }, []);
 
   const validate = () => {
     let obj = {};
@@ -93,301 +99,65 @@ export default function Dashboard() {
         </button>
       </div>
       <div style={{ display: "flex", marginLeft: "11.5rem" }} className="row">
-        <div className="col-md-4">
-          <div
-            class="card"
-            style={{ width: "14.1rem", border: "1px solid black" }}
-          >
-            <div style={{ display: "flex", justifyContent: "center" }}>
-              <img
-                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyYuVsyAh_BIt_gT6a-dYbOVa9RztXdsrOZA&usqp=CAU"
-                class="card-img-top"
-                alt="..."
-                style={{ width: "5rem" }}
-              />
-              <span style={{ marginLeft: "6.9rem", marginTop: "0.5rem" }}>
+        {list?.map((itm) => (
+          <div className="col-md-4" style={{ marginTop: "1rem" }}>
+            <div
+              class="card"
+              style={{ width: "14.1rem", border: "1px solid black" }}
+            >
+              <div style={{ display: "flex", justifyContent: "center" }}>
                 <img
-                  src="https://tse3.mm.bing.net/th?id=OIP.etkSPgPxE8JWuqIdxrRi9AHaHa&pid=Api&P=0&h=180"
-                  style={{
-                    wiodth: "1rem",
-                    height: "1.1rem",
-                    cursor: "pointer",
-                  }}
-                //   data-bs-toggle="modal"
-                //   data-bs-target="#exampleModal11"
-                ></img>
-              </span>
-            </div>
-
-{/* <div  class="modal fade"
-        id="exampleModal11"
-        tabindex="-1"
-        aria-labelledby="exampleModalLabel1"
-        aria-hidden="true">
- hello</div> */}
-
-            <div class="card-body">
-              <div>
-                <b>Name: </b>Ayushi
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyYuVsyAh_BIt_gT6a-dYbOVa9RztXdsrOZA&usqp=CAU"
+                  class="card-img-top"
+                  alt="..."
+                  style={{ width: "5rem" }}
+                />
+                <span style={{ marginLeft: "5rem", cursor: "pointer" }}  onClick={() => {
+            deleteUser();
+          }}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    class="bi bi-archive-fill"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M12.643 15C13.979 15 15 13.845 15 12.5V5H1v7.5C1 13.845 2.021 15 3.357 15h9.286zM5.5 7h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1 0-1zM.8 1a.8.8 0 0 0-.8.8V3a.8.8 0 0 0 .8.8h14.4A.8.8 0 0 0 16 3V1.8a.8.8 0 0 0-.8-.8H.8z" />
+                  </svg>
+                </span>
+                <span style={{ marginLeft: "1rem", cursor: "pointer" }}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    class="bi bi-pen-fill"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001z" />
+                  </svg>
+                </span>
               </div>
-              <div>
-                <b>Phone No: </b>8745677867
-              </div>
-              <div>
-                <b>Address: </b> Noida
+
+              <div class="card-body">
+                <div>
+                  <b>Name: </b>
+                  {itm.name}
+                </div>
+                <div>
+                  <b>Phone No: </b>
+                  {itm.phoneNumber}
+                </div>
+                <div>
+                  <b>Address: </b> {itm.address}
+                </div>
               </div>
             </div>
           </div>
-        </div>
-
-        <div className="col-md-4">
-          <div
-            class="card"
-            style={{ width: "14.1rem", border: "1px solid black" }}
-          >
-            <div style={{ display: "flex", justifyContent: "center" }}>
-              <img
-                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyYuVsyAh_BIt_gT6a-dYbOVa9RztXdsrOZA&usqp=CAU"
-                class="card-img-top"
-                alt="..."
-                style={{ width: "5rem" }}
-              />
-              <span style={{ marginLeft: "6.9rem", marginTop: "0.5rem" }}>
-                <img
-                  src="https://tse3.mm.bing.net/th?id=OIP.etkSPgPxE8JWuqIdxrRi9AHaHa&pid=Api&P=0&h=180"
-                  style={{
-                    wiodth: "1rem",
-                    height: "1.1rem",
-                    cursor: "pointer",
-                  }}
-                ></img>
-              </span>
-            </div>
-
-            <div class="card-body">
-              <div>
-                <b>Name: </b>Ayushi
-              </div>
-              <div>
-                <b>Phone No: </b>8745677867
-              </div>
-              <div>
-                <b>Address: </b> Noida
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="col-md-4">
-          <div
-            class="card"
-            style={{ width: "14.1rem", border: "1px solid black" }}
-          >
-            <div style={{ display: "flex", justifyContent: "center" }}>
-              <img
-                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyYuVsyAh_BIt_gT6a-dYbOVa9RztXdsrOZA&usqp=CAU"
-                class="card-img-top"
-                alt="..."
-                style={{ width: "5rem" }}
-              />
-              <span style={{ marginLeft: "6.9rem", marginTop: "0.5rem" }}>
-                <img
-                  src="https://tse3.mm.bing.net/th?id=OIP.etkSPgPxE8JWuqIdxrRi9AHaHa&pid=Api&P=0&h=180"
-                  style={{
-                    wiodth: "1rem",
-                    height: "1.1rem",
-                    cursor: "pointer",
-                  }}
-                ></img>
-              </span>
-            </div>
-            <div class="card-body">
-              <div>
-                <b>Name: </b>Ayushi
-              </div>
-              <div>
-                <b>Phone No: </b>8745677867
-              </div>
-              <div>
-                <b>Address: </b> Noida
-              </div>
-            </div>
-          </div>
-        </div>
+        ))}
       </div>
-      <i class="bi bi-three-dots-vertical"></i>
-      {/* next row */}
-      <div
-        style={{ display: "flex", marginLeft: "11.5rem", marginTop: "1rem" }}
-        className="row"
-      >
-        <div className="col-md-4">
-          <div
-            class="card"
-            style={{ width: "14.1rem", border: "1px solid black" }}
-          >
-            <div style={{ display: "flex", justifyContent: "center" }}>
-              <img
-                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyYuVsyAh_BIt_gT6a-dYbOVa9RztXdsrOZA&usqp=CAU"
-                class="card-img-top"
-                alt="..."
-                style={{ width: "5rem" }}
-              />
-            </div>
 
-            <div class="card-body">
-              <div>
-                <b>Name: </b>Ayushi
-              </div>
-              <div>
-                <b>Phone No: </b>8745677867
-              </div>
-              <div>
-                <b>Address: </b> Noida
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="col-md-4">
-          <div
-            class="card"
-            style={{ width: "14.1rem", border: "1px solid black" }}
-          >
-            <div style={{ display: "flex", justifyContent: "center" }}>
-              <img
-                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyYuVsyAh_BIt_gT6a-dYbOVa9RztXdsrOZA&usqp=CAU"
-                class="card-img-top"
-                alt="..."
-                style={{ width: "5rem" }}
-              />
-            </div>
-            <div class="card-body">
-              <div>
-                <b>Name: </b>Ayushi
-              </div>
-              <div>
-                <b>Phone No: </b>8745677867
-              </div>
-              <div>
-                <b>Address: </b> Noida
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="col-md-4">
-          <div
-            class="card"
-            style={{ width: "14.1rem", border: "1px solid black" }}
-          >
-            <div style={{ display: "flex", justifyContent: "center" }}>
-              <img
-                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyYuVsyAh_BIt_gT6a-dYbOVa9RztXdsrOZA&usqp=CAU"
-                class="card-img-top"
-                alt="..."
-                style={{ width: "5rem" }}
-              />
-            </div>
-            <div class="card-body">
-              <div>
-                <b>Name: </b>Ayushi
-              </div>
-              <div>
-                <b>Phone No: </b>8745677867
-              </div>
-              <div>
-                <b>Address: </b> Noida
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      {/* next row */}
-      <div
-        style={{ display: "flex", marginLeft: "11.5rem", marginTop: "1rem" }}
-        className="row"
-      >
-        <div className="col-md-4">
-          <div
-            class="card"
-            style={{ width: "14.1rem", border: "1px solid black" }}
-          >
-            <div style={{ display: "flex", justifyContent: "center" }}>
-              <img
-                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyYuVsyAh_BIt_gT6a-dYbOVa9RztXdsrOZA&usqp=CAU"
-                class="card-img-top"
-                alt="..."
-                style={{ width: "5rem" }}
-              />
-            </div>
-
-            <div class="card-body">
-              <div>
-                <b>Name: </b>Ayushi
-              </div>
-              <div>
-                <b>Phone No: </b>8745677867
-              </div>
-              <div>
-                <b>Address: </b> Noida
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="col-md-4">
-          <div
-            class="card"
-            style={{ width: "14.1rem", border: "1px solid black" }}
-          >
-            <div style={{ display: "flex", justifyContent: "center" }}>
-              <img
-                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyYuVsyAh_BIt_gT6a-dYbOVa9RztXdsrOZA&usqp=CAU"
-                class="card-img-top"
-                alt="..."
-                style={{ width: "5rem" }}
-              />
-            </div>
-            <div class="card-body">
-              <div>
-                <b>Name: </b>Ayushi
-              </div>
-              <div>
-                <b>Phone No: </b>8745677867
-              </div>
-              <div>
-                <b>Address: </b> Noida
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="col-md-4">
-          <div
-            class="card"
-            style={{ width: "14.1rem", border: "1px solid black" }}
-          >
-            <div style={{ display: "flex", justifyContent: "center" }}>
-              <img
-                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyYuVsyAh_BIt_gT6a-dYbOVa9RztXdsrOZA&usqp=CAU"
-                class="card-img-top"
-                alt="..."
-                style={{ width: "5rem" }}
-              />
-            </div>
-            <div class="card-body">
-              <div>
-                <b>Name: </b>Ayushi
-              </div>
-              <div>
-                <b>Phone No: </b>8745677867
-              </div>
-              <div>
-                <b>Address: </b> Noida
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
       <div
         class="modal fade"
         id="exampleModal"
@@ -461,19 +231,23 @@ export default function Dashboard() {
             <div class="modal-footer">
               <button
                 type="button"
+                data-bs-dismiss="modal"
                 style={{ backgroundColor: "black", color: "white" }}
                 onClick={() => {
                   if (validate()) {
                     // localStorage.setItem()
-                    if(localStorage.getItem('list')){
-                      let l = localStorage.getItem('list');
+                    let l = null;
+                    if (localStorage.getItem("list")) {
+                      l = JSON.parse(localStorage.getItem("list"));
                       l.push(data);
-                      localStorage.setItem('list',l);
+                      localStorage.setItem("list", JSON.stringify(l));
                     } else {
-                      let l = [];
+                      l = [];
                       l.push(data);
-                      localStorage.setItem('list',l);
+                      localStorage.setItem("list", JSON.stringify(l));
                     }
+                    // console.log(data, l)
+                    setlist(JSON.parse(localStorage.getItem("list")));
                     navigate("/dashboard");
                   }
                 }}
@@ -484,17 +258,39 @@ export default function Dashboard() {
           </div>
         </div>
       </div>
-      <div style={{display:"flex",justifyContent:"center",marginTop:"1rem"}}>
-      <nav aria-label="Page navigation example">
-  <ul class="pagination">
-    <li class="page-item"><a class="page-link" href="#">Previous</a></li>
-    <li class="page-item"><a class="page-link" href="#">1</a></li>
-    <li class="page-item"><a class="page-link" href="#">2</a></li>
-    <li class="page-item"><a class="page-link" href="#">3</a></li>
-    <li class="page-item"><a class="page-link" href="#">Next</a></li>
-  </ul>
-</nav>
-</div>
+      <div
+        style={{ display: "flex", justifyContent: "center", marginTop: "1rem" }}
+      >
+        <nav aria-label="Page navigation example">
+          <ul class="pagination">
+            <li class="page-item">
+              <a class="page-link" href="#">
+                Previous
+              </a>
+            </li>
+            <li class="page-item">
+              <a class="page-link" href="#">
+                1
+              </a>
+            </li>
+            <li class="page-item">
+              <a class="page-link" href="#">
+                2
+              </a>
+            </li>
+            <li class="page-item">
+              <a class="page-link" href="#">
+                3
+              </a>
+            </li>
+            <li class="page-item">
+              <a class="page-link" href="#">
+                Next
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </>
   );
 }
